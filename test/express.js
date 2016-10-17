d1568b8421ed6f5422e16abbdbfda3566197f423
Brian Doane <brianmdoane@gmail.com>
2016-10-17 11:12:30 -0500

    basic supertest test for Hello Nurse and server.js for route testing, with status code

--- test/express.js
+++ test/express.js
@@ -1,16 +1,14 @@
 'use strict'
 
 const request = require('supertest')
-const app = require('../server.js')
+const app = require('../server')
 
-// Using supertest
-describe('', () => {
-	it('should respond with Hello Nurse', () => {
-		request(app)
-			.get('/')
-			.expect(200)
-			.expect('Hello Nurse')
-			.end(done)
-		// res.send === 'Hello Nurse'
-	})
+describe('express', () => {
+  it('should respond with Hello Nurse', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Hello Nurse')
+      .end(done)
+  })
 })
