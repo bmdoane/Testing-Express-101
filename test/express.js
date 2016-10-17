9484f0f19d67d99ab285c4c070adffae838ff689
Brian Doane <brianmdoane@gmail.com>
2016-10-17 13:49:09 -0500

    added further tests with supertest and chai

--- test/express.js
+++ test/express.js
@@ -1,14 +1,39 @@
 'use strict'
 
+const { property } =require('chai').assert
 const request = require('supertest')
 const app = require('../server')
 
 describe('express', () => {
-  it('should respond with Hello Nurse', (done) => {
+  // it('should respond with Hello Nurse', (done) => {
+  //   request(app)
+  //     .get('/')
+  //     .expect(200)
+  //     .expect('Hello Nurse')
+  //     .end(done)
+  // }) 
+  // should be json
+  it('`GET /` should respond with JSON', (done) => {
     request(app)
       .get('/')
       .expect(200)
-      .expect('Hello Nurse')
+      .expect('Content-Type', /json/)
+      .end(done)
+  })
+
+  it('`POST /addresses` should respond with 201 and JSON with an id', (done) => {
+    request(app)
+      .post('/addresses')
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .expect(res => property(res.body, '_id'))
+      .end(done)
+  })
+
+  it('`DELETE /addresses/123` should respond with 204', (done) => {
+    request(app)
+      .delete('/addresses/123')
+      .expect(204)
       .end(done)
   })
 })
